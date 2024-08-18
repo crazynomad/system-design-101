34c1469c058eecc74682083576583451482c1764
Burn <burn.wang@gmail.com>
2024-08-18 12:39:21 +0800

    修正了上一页/下一页的问题

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -1,25 +1,33 @@
 import { defineConfig } from 'vitepress'
 import themeConfig from './themeConfig.json'
 
-// https://vitepress.dev/reference/site-config
+// 遍历 themeConfig.sidebar 数组，元素如果有 link 属性，且其以不是以/开始， 则在前面加上 /
+// 如果元素中有 items 属性，则遍历 items 数组，递归调用 processSidebar
+function processSidebar(sidebar) {
+  sidebar.forEach(item => {
+    if (item.link && !item.link.startsWith('/')) {
+      item.link = '/' + item.link
+    }
+
+    if (item.items) {
+      processSidebar(item.items)
+    }
+  })
+}
+processSidebar(themeConfig.sidebar)
 export default defineConfig({
   title: "System Design 101",
   description: "Explain complex systems using visuals and simple terms.",
   tagline: "Whether you're preparing for a System Design Interview or you simply want to understand how systems work beneath the surface, we hope this repository will help you achieve that.",
-  themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
+  themeConfig: {    
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: 'Get Started', link: '/toc' }
     ],
 
-    "sidebar": themeConfig.sidebar,
-    docFooter: {
-      prev: '上一页',
-      next: '下一页'
-    },
+    "sidebar": themeConfig.sidebar,    
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+      { icon: 'github', link: 'https://github.com/crazynomad/system-design-101' }
     ]
   }
 })
