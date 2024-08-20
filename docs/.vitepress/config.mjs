cdf8a6d5db44991cb2c6cbc390631ead87d3f1d5
Burn <burn.wang@gmail.com>
2024-08-20 13:22:40 +0800

    config update for vitepress

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -19,6 +19,10 @@ export default defineConfig({
   title: "System Design 101",
   description: "Explain complex systems using visuals and simple terms.",
   tagline: "Whether you're preparing for a System Design Interview or you simply want to understand how systems work beneath the surface, we hope this repository will help you achieve that.",
+  sitemap: {
+    hostname: 'https://arch.starteam.work',
+    lastmodDateOnly: true
+  },
   themeConfig: {    
     nav: [
       { text: 'Home', link: '/' },
@@ -28,10 +32,6 @@ export default defineConfig({
     "sidebar": themeConfig.sidebar,    
     socialLinks: [
       { icon: 'github', link: 'https://github.com/crazynomad/system-design-101' }
-    ],
-    sitemap: {
-      hostname: 'https://arch.starteam.work',
-      lastmodDateOnly: true
-    }
+    ]    
   }
 })
